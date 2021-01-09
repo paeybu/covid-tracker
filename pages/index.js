d4e4c1ed99d2c474bd3590ef57f15119610193a2
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2021-01-10 00:04:54 +0700

    covid tracker

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,114 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({ dailyData, cases }) {
+  const {
+    UpdateDate,
+    Confirmed,
+    NewConfirmed,
+    Recovered,
+    NewRecovered,
+    Hospitalized,
+    NewHospitalized,
+    Deaths,
+    NewDeaths,
+  } = dailyData;
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  const todayFormat = dayjs().format('YYYY-MM-DD');
+  const todayCases = cases.Data.filter(
+    (data) => data.ConfirmDate.substr(0, 10) === todayFormat
+  );
+  const nonthaburiTodayCases = todayCases.filter(
+    (data) => data.Province === 'นนทบุรี'
+  );
+  const bkkTodayCases = todayCases.filter((data) =>
+    data.Province.includes('กรุงเทพ')
+  );
+  console.log(nonthaburiTodayCases);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  const confirmed = Number.parseInt(Confirmed, 10);
+  const newConfirmed = Number.parseInt(NewConfirmed, 10);
+  const recovered = Number.parseInt(Recovered, 10);
+  const newRecovered = Number.parseInt(NewRecovered, 10);
+  const hospitalized = Number.parseInt(Hospitalized, 10);
+  const newHospitalized = Number.parseInt(NewHospitalized, 10);
+  const deaths = Number.parseInt(Deaths, 10);
+  const newDeaths = Number.parseInt(NewDeaths, 10);
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+  const Card = ({ title, content }) => (
+    <div className='shadow p-5'>
+      <p>{title}</p>
+      <p className='font-bold'>{content}</p>
     </div>
-  )
+  );
+
+  return (
+    <div className='container mx-auto my-5 text-lg'>
+      <p className='text-sm text-gray-400 mb-5'>
+        Update โควิดประจำวันที่ {UpdateDate.substr(0, 10)} เวลา{' '}
+        {UpdateDate.substr(10, UpdateDate.length)}
+      </p>
+      <div className='grid grid-cols-3 gap-8 text-center'>
+        <Card
+          title='ติดเชื้อสะสม'
+          content={`${confirmed} (${
+            newConfirmed > 0 ? '+' : ''
+          }${newConfirmed})`}
+        />
+        <Card
+          title='หายแล้ว'
+          content={`${recovered} (${
+            newRecovered > 0 ? '+' : ''
+          }${newRecovered})`}
+        />
+        <Card
+          title='รักษาอยู่ใน ร.พ.'
+          content={`${hospitalized} (${
+            newHospitalized > 0 ? '+' : ''
+          }${newHospitalized})`}
+        />
+        <Card
+          title='เสียชีวิต'
+          content={`${deaths} (${newDeaths > 0 ? '+' : ''}${newDeaths})`}
+        />
+        <Card
+          title='ผู้เสียชีวิตทั้งหมด / ผู้ติดเชื้อสะสม'
+          content={`${((deaths / confirmed) * 100).toFixed(2)}%`}
+        />
+      </div>
+      <div className='grid grid-cols-3 gap-8 text-center mt-8'>
+        <div className='shadow p-5'>
+          <p className='font-bold'>จังหวัดนนทบุรี</p>
+          <p>
+            ผู้ติดใหม่วันนี้{' '}
+            <span className='font-bold'>{nonthaburiTodayCases.length}</span>
+          </p>
+          {nonthaburiTodayCases.map(({ District }) => (
+            <p>อำเภอ: {District}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Home.propTypes = {
+  dailyData: PropTypes.func,
+};
+
+Home.defaultProps = {
+  dailyData: {},
+};
+
+export async function getServerSideProps() {
+  const res1 = await axios.get('https://covid19.th-stat.com/api/open/today');
+  const res2 = await axios.get('https://covid19.th-stat.com/api/open/cases');
+
+  return {
+    props: {
+      dailyData: res1.data,
+      cases: res2.data,
+    },
+  };
 }
